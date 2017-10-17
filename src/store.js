aa6f021a5dfa8f0cd60a03c4e7bf290726b2d4e5
btzr-io <btzr.io@gmail.com>
2017-10-16 22:58:30 -0600

    create store

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import reducers from './reducers.js';
+
+// Initial state
+const initialState = {
+    todos: [],
+};
+
+// Create store
+const store = createStore(reducers, initialState);
+
+export default store;
